58f768c59757fc5d6044924b79e8b49faa18f106
dayana <yedarovega4@gmail.com>
2017-07-12 10:10:08 -0500

    hola

---
+++ ejemplo (2).js
@@ -0,0 +1,10 @@
+alert("Hola mundo");
+if (confirm("Estas contento en clase?")){
+	//lo que se hace si se cumple la condicion
+	alert("Me caes bien")
+	} else {
+		//lo que se hace si NO se cumple la condicion
+        alert("Mejor quede durmiendo en la casa");
+	}
+
+	
